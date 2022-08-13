bc20180a41debf7a56d6d1f28fcc8fd68e351607
HikmetMUT <hikmetmut@gmail.com>
2022-08-13 11:06:12 +0300

    13/08/2022

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -14,23 +16,38 @@ export class ProductComponent implements OnInit {
 
 products:Product[] = [];
 dataLoaded=false;
+filterText:string="";
 
 
  
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute, private toastrService:ToastrService) { }
 
   ngOnInit(): void {
+ this.activatedRoute.params.subscribe(params=>{
+  if(params["categoryId"]){
+    this.getProductsByCategory(params["categoryId"]);
+  }
+  else{
     this.getProducts();
+  }
+ })
   }
   getProducts(){
     this.productService.getProducts().subscribe(response=>{
       this.products=response.data
       this.dataLoaded=true;
     })
-      console.log();
   }
-
-
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
+    })
+  }
+ 
+  addToCart(product:Product){
+    this.toastrService.success("sepete Eklendi",product.productName)
+  }
 
 }
 
---
+++ src/app/pipes/vat-added.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'vatAdded'
+})
+export class VatAddedPipe implements PipeTransform {
+
+  transform(value: number, rate:Number): number {
+    return value+(value*0.50);
+  }
+
+}
